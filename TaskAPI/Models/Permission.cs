b903dbd1dcb8ff02e8d70b5b9a9a702fe616235b
NikolaDž <nikola.dzidic@edu.fit.ba>
2022-05-23 10:36:11 +0200

    Model creation

---
+++ TaskAPI/Models/Permission.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskAPI.Models
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
---
+++ TaskAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        [StringLength(20)]
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int PermissionId { get; set; }
+        public Permission? Permission { get; set; }
+    }
+}
