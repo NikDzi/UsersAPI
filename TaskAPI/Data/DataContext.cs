5f118738048e966062fa2a83f4f9695fec27814b
NikolaDž <nikola.dzidic@edu.fit.ba>
2022-05-25 03:07:59 +0200

    added database creator

--- TaskAPI/Data/DataContext.cs
+++ TaskAPI/Data/DataContext.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using TaskAPI.Models;
 
 namespace TaskAPI.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            if (databaseCreator != null)
+            {
+                if (!databaseCreator.CanConnect())
+                {
+                    databaseCreator.Create();
+                }
+                if (!databaseCreator.HasTables())
+                {
+                    databaseCreator.CreateTables();
+                }
+            }
+        }
         public DbSet<User> Users { get; set; }
         public DbSet<Permission> Permissions { get; set; }
     }
